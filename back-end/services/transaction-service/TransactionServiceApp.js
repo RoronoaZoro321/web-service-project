a4c5db3c89dd1c64ff2737e8ff49bb180e078f18
PomPerWoW <chatthanes22@gmail.com>
2024-08-07 23:26:46 +0700

    refactor wsdl file

--- back-end/services/transaction-service/TransactionServiceApp.js
+++ back-end/services/transaction-service/TransactionServiceApp.js
@@ -1,11 +1,5 @@
 const express = require("express");
-const helmet = require("helmet");
-const morgan = require("morgan");
-const bodyParser = require("body-parser");
-
-const AppError = require("../../common/utils/appError");
-const globalErrorHandler = require("../../common/controllers/errorController");
-const startSoapServer = require("./controllers/transactionController"); // Import SOAP server setup
+const startSoapServer = require("./controllers/transactionController");
 
 const app = express();
 
