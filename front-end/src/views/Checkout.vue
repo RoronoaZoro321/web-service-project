e396a77ff0407f592f660eaf6715ba1f9160ba09
PomPerWoW <chatthanes22@gmail.com>
2024-08-07 23:43:32 +0700

    delete dangling front-end

--- front-end/src/views/Checkout.vue
+++
@@ -1,8 +0,0 @@
-<template>
-    <div class="mx-auto my-32 border-solid border-2 border-slate-100 p-6 rounded-lg space-y-4 max-w-96">
-        <CheckoutComponent/>
-    </div>
-</template>
-<script setup>
-import CheckoutComponent from '../components/CheckoutComponent.vue'
-</script>
