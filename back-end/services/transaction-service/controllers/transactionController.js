c0fbcfe2509cf406e35b7a0df47c8f827ce7008d
lollipop <hastyfasd@gmai.com>
2024-08-07 15:28:19 +0700

    top finish

--- back-end/services/transaction-service/controllers/transactionController.js
+++ back-end/services/transaction-service/controllers/transactionController.js
@@ -1,5 +1,6 @@
 const Transaction = require("../../../db/models/transactionModel");
 const Account = require("../../../db/models/accountModel");
+const Topup = require("../../../db/models/topupModel");
 const catchAsync = require("../../../common/utils/catchAsync");
 const AppError = require("../../../common/utils/appError");
 
@@ -83,6 +84,66 @@ exports.getAllTransactionsByAccountId = catchAsync(async (req, res, next) => {
         results: transactions.length,
         data: {
             transactions,
+        },
+    });
+});
+
+exports.topup = catchAsync(async (req, res, next) => {
+    const { accountId, code } = req.body;
+    const account = await Account.findById(accountId);
+
+    if (!account) {
+        return next(new AppError("Account not found", 400));
+    }
+
+    const topup = await Topup.findOne({ code });
+
+    if (!topup) {
+        return next(new AppError("Invalid code", 400));
+    }
+
+    if (topup.isUsed) {
+        return next(new AppError("Code already used", 400));
+    }
+
+    account.balance += topup.amount;
+    topup.isUsed = true;
+    await account.save();
+    await topup.save();
+
+    res.status(200).json({
+        status: "success",
+        data: {
+            message: "Topup successful",
+            account,
+        },
+    });
+});
+    
+exports.createTopup = catchAsync(async (req, res, next) => {
+    const { code, amount } = req.body;
+    const topup = await Topup.create({
+        code,
+        amount,
+    });
+
+    res.status(201).json({
+        status: "success",
+        data: {
+            topup,
+        },
+    });
+});
+
+
+exports.getAllTopup = catchAsync(async (req, res, next) => {
+    const topups = await Topup.find();
+
+    res.status(200).json({
+        status: "success",
+        results: topups.length,
+        data: {
+            topups,
         },
     });
 });
